f7d03311520d9fa2b1cbf943532e67582073d434
Vadym <v.rudenko106@gmail.com>
2024-02-09 12:16:06 +0200

    new test added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,9 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test("navigate wikipedia", async({page}) => {
+  await page.goto("https://www.wikipedia.org/")
+
+  await expect(page.url()).toContain("wikipedia")
+})
